a6f537c399da00084f583f9b0c05287c913fc99c
AStappenbeck13 <astappenbeck17@ladueschools.net>
2023-03-08 01:05:04 +0000

    3/7/2023

---
+++ Fish.java
@@ -0,0 +1,17 @@
+/*
+ * Activity 4.9.3
+ */
+public class Fish extends LakeObject
+{
+    @Override
+    public String say()
+    {
+      return "You now have a fish!";
+    }
+
+    @Override
+    public int getCost()
+    {
+      return (super.getCost() * super.getWeight());
+    }
+}
